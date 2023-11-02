d219e1c09175cad21620c36a1d3375cd58a4589b
Sae Sasaki <s22c1053pw@s.chibakoudai.jp>
2023-11-02 13:54:12 +0900

    tsukareta

--- test.bash
+++ test.bash
@@ -1,18 +1,17 @@
 #!/bin/bash
-#
-#
-
+# SPDX-FileCopyrightText: 2023 Sae Sasaki 
+# SPDX-License-Identifier: BSD-3-Clause
 ng (){
-	echo NG at Line $1
-	ret=1
+      echo NG at Line $1
+      res=1
 }
 
-ret=0
+res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}    　
+[ "$res" = 0 ] && echo OK
 
-[ "$ret" = 0 ] && echo OK
-exit $ret
+exit $res
