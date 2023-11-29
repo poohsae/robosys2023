1b4aa5e9e44a0be729fc00f063f6f11faa2da637
poohsae <148409408+poohsae@users.noreply.github.com>
2023-11-29 17:45:30 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -13,6 +13,15 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}}
 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" ~ 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo |./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
 
