2197748bd07f425ed6f662dc37dc179e347af262
Sae Sasaki <s22c1053pw@s.chibakoudai.jp>
2023-11-29 16:16:28 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -xv 
 # SPDX-FileCopyrightText: 2023 Sae Sasaki 
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -11,17 +11,8 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}}
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINONE}
 [ "$res" = 0 ] && echo OK
-
 exit $res
 
