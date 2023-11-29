c3a39c5dedcd361bf0b520229e0a570e0345dcde
Sae Sasaki <s22c1053pw@s.chibakoudai.jp>
2023-11-29 16:06:57 +0900

    test

--- test.bash
+++ test.bash
@@ -1,6 +1,7 @@
-#!/bin/bash -vx
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2023 Sae Sasaki 
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng (){
       echo NG at Line $1
       res=1
@@ -10,8 +11,16 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15  ] || ng ${LINENO}    　
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINONE}
 [ "$res" = 0 ] && echo OK
 
 exit $res
