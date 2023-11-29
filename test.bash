46ac8235a84da46da38fe57dd97baa31fb2cac39
Sae Sasaki <s22c1053pw@s.chibakoudai.jp>
2023-11-29 13:45:31 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -vx
 # SPDX-FileCopyrightText: 2023 Sae Sasaki 
 # SPDX-License-Identifier: BSD-3-Clause
 ng (){
@@ -11,7 +11,8 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ] || ng ${LINENO}    　
+[ "${out}" = 15  ] || ng ${LINENO}    　
 [ "$res" = 0 ] && echo OK
 
 exit $res
+
